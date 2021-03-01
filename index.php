512f0cbd8fc95dcf20cb2dc07d26dbb87224376f
user <user@uit.local>
2021-03-01 13:34:20 +0300

    Rabota_s_usl

--- index.php
+++ index.php
@@ -27,17 +27,20 @@ $user_name = 'Lesha718'; // укажите здесь ваше имя
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-         <?php if ($is_auth):?>
+         <?php if ($is_auth==1):?>
         <div class="user-menu__image">
            <img src="img/user.jpg" width="40" height="40" alt="Пользователь">
         </div>
+         <div>
+             <p> <?php echo($user_name); ?></p>
+         </div>
             <?php else: ?>
             <ul class="user-menu__list">
                 <li class="user-menu__item">
                     <a href="#">Вход</a>
                 </li>
             </ul>
-            <? php endif;?>
+            <?php endif;?>
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
 
         </nav>
