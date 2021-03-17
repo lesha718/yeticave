cb9b61cf7be9789641d4c30ca2f05a54976ae3f6
user <user@uit.local>
2021-03-17 10:08:13 +0300

    zaprosi

--- templates/index.php
+++ templates/index.php
@@ -24,7 +24,7 @@
                     </div>
                     <div class="lot__info">
                         <span class="lot__category"><?php echo($categories_list[$val['category']]); ?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?php echo($val['name']); ?></a></h3>
+                        <h3 class="lot__title"><a class="text-link" href="templates/php.lot"><?php echo($val['name']); ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount"><?php echo($val['count']);?></span>
